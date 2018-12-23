a23b5e95d88a1c4b057250eb3320bc0f927ab7ef
navin106 <sandula.navin@msitprogram.net>
2018-12-23 18:33:22 +0530

    first assignment

--- CNF-assignments/m8/TCP_CurrencyConverter/tcpServerConverter.py
+++ CNF-assignments/m8/TCP_CurrencyConverter/tcpServerConverter.py
@@ -3,6 +3,7 @@ remote_server = "google.com"
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as soc: 
     soc.connect((remote_server, 80))
     host = soc.getsockname()[0]
+    
 def Main():
     # print(host)
     # host = '10.1.135.71'
