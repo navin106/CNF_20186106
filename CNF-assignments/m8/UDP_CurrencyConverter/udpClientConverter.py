9a847450230bb83793a8f3e2cc7fc376c3fe3e2d
navin106 <sandula.navin@msitprogram.net>
2018-12-23 18:37:39 +0530

    udp_currency

--- CNF-assignments/m8/UDP_CurrencyConverter/udpClientConverter.py
+++ CNF-assignments/m8/UDP_CurrencyConverter/udpClientConverter.py
@@ -1,17 +1,20 @@
 import socket
-
+remote_server = "google.com"
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as soc: 
+    soc.connect((remote_server, 80))
+    host = soc.getsockname()[0]
 def Main():
-	host = '10.10.9.47'
-	port = 5101
-	server = ('10.10.9.47',5112)
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	s.bind((host, port))
-	message = input('-->')
-	while True:
-		s.sendto(message.encode(), server)
-		data, addr = s.recvfrom(1024)
-		print('Data Recieved from Server: ' + str(data.decode()))
-		message = input('-->')
-	s.close()
+    # host = '10.10.9.47'
+    port = 5101
+    server = ('10.10.9.47',5112)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((host, port))
+    message = input('-->')
+    while True:
+        s.sendto(message.encode(), server)
+        data, addr = s.recvfrom(1024)
+        print('Data Recieved from Server: ' + str(data.decode()))
+        message = input('-->')
+    s.close()
 if __name__ == '__main__':
-	Main()
+    Main()
